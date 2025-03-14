ef10d5babce8f2c4db18bd8924c446a3f8c2b51a
phsoftware-corp <heckervirusads+free@gmail.com>
2025-03-14 20:25:45 +0100

    Create Application.py

---
+++ Application.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def lmfao():
+            os.system("msg * operation failed")
+
+root = tk.Tk()
+root.title("your computer has virus")
+open_button = tk.Button(root, text="delete virus", command=open_code_window)
+open_button.pack(pady=10)
+root.mainloop()
